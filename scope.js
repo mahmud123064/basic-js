e2384fc16ae43aa301a53cf7517d5abb64c239da
Md. Mahmud Uz Zaman <zmahmud26@gmail.com>
2024-01-01 11:19:43 +0600

    first commit

---
+++ scope.js
@@ -0,0 +1,12 @@
+var subject = "Javascriptttt";
+
+function learn(topic) {
+    subject = topic;
+    console.log(`I am learning ${topic}`);
+    return;
+}
+
+learn("Javascript")
+
+console.log(`I know ${subject}`);
+
